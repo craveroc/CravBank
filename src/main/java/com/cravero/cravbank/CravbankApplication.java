f1a3aa821a4af91b34a2a87c3b23c74ab26f93d1
Molich <clement.cravero@gmail.com>
2026-04-20 00:24:06 +0200

    init spring

---
+++ src/main/java/com/cravero/cravbank/CravbankApplication.java
@@ -0,0 +1,13 @@
+package com.cravero.cravbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CravbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CravbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cravero/cravbank/CravbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cravero.cravbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CravbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
